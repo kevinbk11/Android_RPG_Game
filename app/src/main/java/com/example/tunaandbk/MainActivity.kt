08b79f0ac7834f22532c47e553e0d8059e72aa4b
kevinbk11 <r91090701@gmail.com>
2021-10-13 08:45:39 +0800

    i

--- app/src/main/java/com/example/tunaandbk/MainActivity.kt
+++ app/src/main/java/com/example/tunaandbk/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.tunaandbk
 
+import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test
     }
 }
