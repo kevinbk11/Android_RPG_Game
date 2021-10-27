8a3411dbe0a0edf85152ce27564049f5571fbd1b
burstblacktuna <59651678+burstblacktuna@users.noreply.github.com>
2021-10-28 00:32:59 +0800

    map

---
+++ app/src/main/java/com/example/tunaandbk/BigMap.kt
@@ -0,0 +1,13 @@
+package com.example.tunaandbk
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tunaandbk.System.FileReadOrWrite
+
+class BigMap : AppCompatActivity(), FileReadOrWrite {
+
+
+
+
+
+}
