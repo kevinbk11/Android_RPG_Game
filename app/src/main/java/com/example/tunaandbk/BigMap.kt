92660e88a9680499135095cedd48972687491350
kevinbk11 <r91090701@gmail.com>
2021-10-29 00:26:19 +0800

    不規則按鈕完成

--- app/src/main/java/com/example/tunaandbk/BigMap.kt
+++ app/src/main/java/com/example/tunaandbk/BigMap.kt
@@ -1,6 +1,9 @@
 package com.example.tunaandbk
 
 import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.Bitmap.createBitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
@@ -8,15 +11,35 @@ import android.view.MotionEvent
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.graphics.drawable.toBitmap
 import com.example.tunaandbk.System.FileReadOrWrite
+import com.example.tunaandbk.System.checkColor
+import kotlinx.android.synthetic.main.activity_big_map.*
 
 class BigMap : AppCompatActivity(), FileReadOrWrite {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_big_map)
+        imageButton.setOnTouchListener(object: View.OnTouchListener{
+            override fun onTouch(view: View?, e: MotionEvent?): Boolean {
+                if(checkColor(imageButton,e!!))
+                {
+                    //TODO
+                    return true
+                }
+                else return false
+            }
+        })
     }
     fun t(view: View,e:MotionEvent)
     {
         Log.v("testtest","test")
+        /*imageButton.isDrawingCacheEnabled=true
+        val imgbmp = Bitmap.createBitmap(imageButton.drawingCache)
+        val px=imgbmp.getPixel(e.x.toInt(),e.y.toInt())
+        val r = Color.red(px)
+        val g = Color.green(px)
+        val b = Color.blue(px)
+        Log.v("RGB","$r:$g:$b")*/
     }
 }
--- app/src/main/java/com/example/tunaandbk/System/SystemFunction.kt
+++ app/src/main/java/com/example/tunaandbk/System/SystemFunction.kt
@@ -1,7 +1,12 @@
 package com.example.tunaandbk.System
 
+import android.graphics.Color
+import android.view.MotionEvent
 import android.view.View
 import android.view.Window
+import android.widget.ImageButton
+import androidx.core.graphics.drawable.toBitmap
+import kotlinx.android.synthetic.main.activity_big_map.*
 
 fun hideBar(window: Window)
 {
@@ -14,4 +19,14 @@ fun hideBar(window: Window)
                 View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }
+}
+fun checkColor(imageButton:ImageButton,e: MotionEvent):Boolean
+{
+    val imgbmp = imageButton.drawable.toBitmap()
+    val px=imgbmp.getPixel(e!!.x.toInt(),e.y.toInt())
+    val r = Color.red(px)
+    val g = Color.green(px)
+    val b = Color.blue(px)
+    if(r==g && g==b && r==0)return false
+    return true
 }
