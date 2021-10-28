fd5a5e2aa78026a7a22294022f233ad0b29007f9
kevinbk11 <r91090701@gmail.com>
2021-10-28 23:45:58 +0800

    怪物讀寫完成

--- app/src/main/java/com/example/tunaandbk/BigMap.kt
+++ app/src/main/java/com/example/tunaandbk/BigMap.kt
@@ -3,12 +3,20 @@ package com.example.tunaandbk
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.tunaandbk.System.FileReadOrWrite
 
 class BigMap : AppCompatActivity(), FileReadOrWrite {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_big_map)
+    }
+    fun t(view: View,e:MotionEvent)
+    {
+        Log.v("testtest","test")
     }
 }
