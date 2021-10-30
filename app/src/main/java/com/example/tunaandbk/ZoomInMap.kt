a56156abb874d381a8105b66b566b5fb9f56f3c0
burstblacktuna <59651678+burstblacktuna@users.noreply.github.com>
2021-10-30 15:38:40 +0800

    button

---
+++ app/src/main/java/com/example/tunaandbk/ZoomInMap.kt
@@ -0,0 +1,11 @@
+package com.example.tunaandbk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ZoomInMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zoom_in_map)
+    }
+}
