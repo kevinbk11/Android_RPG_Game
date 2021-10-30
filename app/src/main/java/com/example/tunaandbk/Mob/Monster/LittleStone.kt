86e41bcdee2151ae1f794e14cb43c3b26afedb5f
kevinbk11 <r91090701@gmail.com>
2021-10-30 18:03:54 +0800

    :)

---
+++ app/src/main/java/com/example/tunaandbk/Mob/Monster/LittleStone.kt
@@ -0,0 +1,3 @@
+package com.example.tunaandbk.Mob.Monster
+
+class LittleStone:Monster("小石怪",100,10.0)
--- app/src/main/java/com/example/tunaandbk/Mob/Monster/TurnTurnBird.kt
+++ app/src/main/java/com/example/tunaandbk/Mob/Monster/TurnTurnBird.kt
@@ -2,6 +2,6 @@ package com.example.tunaandbk.Mob.Monster
 
 import com.example.tunaandbk.Mob.Skill.MonsterSkill.BirdAttack
 
-object TurnTurnBird:Monster("轉轉鳥",100,10.0) {
+class TurnTurnBird:Monster("轉轉鳥",100,10.0) {
     init{skills.add(BirdAttack(damage))}
 }
